14d2302feeab38baaf09a9428d86d91cf5b51986
patreynf-maker <patrey.nf@gmail.com>
2026-05-15 15:53:15 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/tjts/', // GitHub Pages project page base path
+    base: '/tarjetas/', // GitHub Pages project page base path
     build: {
         outDir: 'docs', // GitHub Pages supports '/' or '/docs' as source, NOT '/dist'
         assetsDir: 'assets',
